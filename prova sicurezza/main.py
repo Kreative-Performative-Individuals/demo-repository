e9f5253182a1dd8135dc7323dc4345752cf4306f
M4rinz <134485532+M4rinz@users.noreply.github.com>
2024-11-17 12:47:32 +0100

    Black reformatted to make the CI pipeline happy

--- prova sicurezza/main.py
+++ prova sicurezza/main.py
@@ -3,10 +3,14 @@ import requests
 # Mock API Key for testing secret scanning
 API_KEY = "12345-abcde-67890-fghij"
 
+
 # Vulnerable library usage (e.g., a deprecated SSL certificate verification method)
 def fetch_data(url):
-    response = requests.get(url, verify=False)  # Trivy should flag "verify=False" as insecure
+    response = requests.get(
+        url, verify=False
+    )  # Trivy should flag "verify=False" as insecure
     return response.text
+
 
 if __name__ == "__main__":
     print(fetch_data("https://example.com"))
